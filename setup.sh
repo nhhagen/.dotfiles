7b57c9e4038a476c37d8d567ae1a25fc423476e9
Niels Henrik Hagen <nhhagen@gmail.com>
2013-09-29 13:35:18 +0200

    Updated setup script and created update script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+
+SYSTEM_TYPE=$(uname)
+
+LINUX='Linux'
+OSX='Darwin'
+
 ln -Ffs ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -Ffs ~/.dotfiles/.vimrc ~/.vimrc
 ln -Ffs ~/.dotfiles/.bash_prompt ~/.bash_prompt
-ln -Ffs ~/.dotfiles/.bashrc ~/.bashrc
-
+if [ "$SYSTEM_TYPE" == "$LINUX" ]; then
+  ln -Ffs ~/.dotfiles/.bashrc_linux ~/.bashrc
+fi
+if [ "$SYSTEM_TYPE" == "$OSX" ]; then
+  ln -Ffs ~/.dotfiles/.bashrc_mac ~/.bashrc
+  ln -Ffs ~/.dotfiles/.bash_profile_mac ~/.bash_profile
+fi
