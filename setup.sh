dbc9324bf1dd293963c8c663532be5fa3ea68c81
Niels Henrik Hagen <nhhagen@gmail.com>
2014-01-15 16:14:17 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ SYSTEM_TYPE=$(uname)
 
 LINUX='Linux'
 OSX='Darwin'
-git submodule update --init --recursive
-git submodule foreach git pull origin master
-git submodule update --init --recursive
 ln -Ffs ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -fs ~/.dotfiles/.vimrc ~/.vimrc
 ln -fs ~/.dotfiles/.vim ~/.vim
--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 git pull
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+git submodule update --init --recursive
 sh ~/.dotfiles/setup.sh
