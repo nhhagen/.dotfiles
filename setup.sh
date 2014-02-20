4fc3d1052d851085386fa2903c6c9c4b8f8104e4
Niels Henrik Hagen <nhhagen@gmail.com>
2014-02-20 12:49:26 +0100

    Added a tmux config

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -Ffs ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -fs ~/.dotfiles/.vimrc ~/.vimrc
 ln -fs ~/.dotfiles/.vim ~/.vim
 ln -fs ~/.dotfiles/.bash_prompt ~/.bash_prompt
+ln -fs ~/.dotfiles/.tmux.conf ~/.tmux.conf
 if [ "$SYSTEM_TYPE" == "$LINUX" ]; then
   ln -Ffs ~/.dotfiles/.bashrc_linux ~/.bashrc
 fi
