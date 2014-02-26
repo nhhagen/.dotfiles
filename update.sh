d96cc20d94ddd7bf60a856f6b617dc1a5c9ec15a
Niels Henrik Hagen <nhhagen@gmail.com>
2014-02-26 01:33:40 +0100

    ?

--- update.sh
+++ update.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+<<<<<<< HEAD
 git pull --rebase &&
 git submodule update --init --recursive &&
 git submodule foreach git pull --rebase origin master &&
 git add -A . &&
 git commit -m "Ran update.sh" &&
 git push &&
+=======
+git pull
+git submodule update --init --recursive
+git submodule foreach git pull origin master
+git add -A .
+git commit -m "Ran update.sh"
+git push
+>>>>>>> parent of a208583... failing update early
 sh ~/.dotfiles/setup.sh
+rm .vim/.vim
 
