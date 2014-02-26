96854d5c9bd60df36420efc357856256ed6b5c42
Niels Henrik Hagen <nhhagen@gmail.com>
2014-02-26 01:36:12 +0100

    failed to remove patch syntax

--- update.sh
+++ update.sh
@@ -6,7 +6,6 @@ git submodule foreach git pull origin master
 git add -A .
 git commit -m "Ran update.sh"
 git push
->>>>>>> parent of a208583... failing update early
 sh ~/.dotfiles/setup.sh
 rm .vim/.vim
 
