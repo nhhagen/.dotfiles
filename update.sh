0b5d65431a18d2296294b52e51a8e6245b1215c9
Niels Henrik Hagen <nhhagen@gmail.com>
2014-02-26 01:28:46 +0100

    Rebase during updates

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-git pull &&
+git pull --rebase &&
 git submodule update --init --recursive &&
-git submodule foreach git pull origin master &&
+git submodule foreach git pull --rebase origin master &&
 git add -A . &&
 git commit -m "Ran update.sh" &&
 git push &&
