a20858392bdecd4215747c774a44dbec0e24666c
Niels Henrik Hagen <nhhagen@gmail.com>
2014-02-26 01:25:54 +0100

    failing update early

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-git pull
-git submodule update --init --recursive
-git submodule foreach git pull origin master
-git add -A .
-git commit -m "Ran update.sh"
-git push
+git pull &&
+git submodule update --init --recursive &&
+git submodule foreach git pull origin master &&
+git add -A . &&
+git commit -m "Ran update.sh" &&
+git push &&
 sh ~/.dotfiles/setup.sh
-rm .vim/.vim
 
