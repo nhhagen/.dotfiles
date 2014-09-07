e769e5617673f31afe0ec6447f99a75c58ef6f43
Niels Henrik Hagen <nhhagen@gmail.com>
2014-09-07 23:34:05 +0200

    automated installation of brew packages on osx

--- setup.sh
+++ setup.sh
@@ -4,6 +4,23 @@ SYSTEM_TYPE=$(uname)
 
 LINUX='Linux'
 OSX='Darwin'
+
+if [ "$SYSTEM_TYPE" == "$OSX" ]; then
+    packages=(
+        vim
+        tmux
+        git
+        git-flow
+        node
+        ack
+        reattach-to-user-namespace
+        wget
+    )
+    for package in ${packages[*]}; do
+        brew install $package
+    done
+fi
+exit
 ln -Ffs ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -fs ~/.dotfiles/.vimrc ~/.vimrc
 ln -fs ~/.dotfiles/.vim ~/.vim && rm -f ~/.dotfiles/.vim/.vim
@@ -19,21 +36,21 @@ ln -fs ~/.dotfiles/.colorsvnrc ~/.colorsvnrc
 #vim +PluginUpdate +qall
 
 if [ "$SYSTEM_TYPE" == "$LINUX" ]; then
-  ln -Ffs ~/.dotfiles/.bashrc_linux ~/.bashrc
+    ln -Ffs ~/.dotfiles/.bashrc_linux ~/.bashrc
 fi
 if [ "$SYSTEM_TYPE" == "$OSX" ]; then
-  ln -Ffs ~/.dotfiles/.bashrc_mac ~/.bashrc
-  ln -Ffs ~/.dotfiles/.bash_profile_mac ~/.bash_profile
-  ln -Ffs ~/.dotfiles/.zshrc ~/.zshrc
-  ln -Ffs ~/.dotfiles/.zsh_prompt ~/.zsh_prompt
+    ln -Ffs ~/.dotfiles/.bashrc_mac ~/.bashrc
+    ln -Ffs ~/.dotfiles/.bash_profile_mac ~/.bash_profile
+    ln -Ffs ~/.dotfiles/.zshrc ~/.zshrc
+    ln -Ffs ~/.dotfiles/.zsh_prompt ~/.zsh_prompt
 
-  ROOT=$(pwd)
-  cd ~/.dotfiles/.vim/bundle/YouCompleteMe/
-  ./install.sh --clang-completer
+    ROOT=$(pwd)
+    cd ~/.dotfiles/.vim/bundle/YouCompleteMe/
+    ./install.sh --clang-completer
 
-  cd ~/.dotfiles/.vim/bundle/tern_for_vim
-  npm install
+    cd ~/.dotfiles/.vim/bundle/tern_for_vim
+    npm install
 
-  cd $ROOT
+    cd $ROOT
 fi
 
