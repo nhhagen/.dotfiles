ee30c78c875ee59ad9650c7757f1219edf0388cb
Niels Henrik Hagen <nhhagen@gmail.com>
2015-10-06 20:21:56 +0200

    Paching Input font

--- powerline-patcher.sh
+++ powerline-patcher.sh
@@ -1,11 +1,11 @@
 fontpatcher="./powerline-fontpatcher/scripts/powerline-fontpatcher"
-source="./Meslo-Font/dist/v1.2.1/Meslo LG DZ v1.2.1"
-target="./Meslo-Font/dist/v1.2.1/"
+source="./Input-Font/Input_Fonts/InputMono/InputMonoCondensed"
+target="./Input-Font/Input_Fonts/InputMono"
 
 brew update
 brew install fontforge
 
-unzip -o "$source.zip" -d "$target"
+# unzip -o "$source.zip" -d "$target"
 for file in "$source/"*.ttf; do
     echo "$file"
     fontforge -script "$fontpatcher" "$file"
