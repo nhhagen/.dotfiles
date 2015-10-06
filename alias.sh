2b107ae6399f25f6a75b0eff3d62103de7058de9
Niels Henrik Hagen <nhhagen@gmail.com>
2015-10-06 20:21:29 +0200

    new alias top-commands

--- alias.sh
+++ alias.sh
@@ -3,3 +3,4 @@
 
 # List the largest files/directory in directory
 alias ducks='du -a * | sort -rn | head'
+alias top-commands="history | awk '{a[\$2]++ } END{for(i in a){print a[i] \" \" i}}'|sort -rn |head -n 20"
