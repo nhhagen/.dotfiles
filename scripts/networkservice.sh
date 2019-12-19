13d292c1753d5e83ba909a59022441b6eaeaa891
Niels Henrik Hagen <nhhagen@gmail.com>
2019-12-19 10:19:37 +0100

    Updates

--- scripts/networkservice.sh
+++ scripts/networkservice.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-setDefaults() {
-  output_tmux=1
-  good_color="green"
-  middle_color="yellow"
-  warn_color="red"
-}
-
-setDefaults
-
 services=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
 
 while read line; do
@@ -28,35 +19,7 @@ while read line; do
 done <<< "$(echo "$services")"
 
 if [[ $currentservice == 'Wi-Fi' ]]; then
-    echo " $(wifi.sh -t)"
-elif [[ $(echo $currentservice | sed -e 's/.*Ethernet.*/Ethernet/') == 'Ethernet' ]]; then
-    speed=$(ifconfig $sdev 2>/dev/null | grep 'media' | sed -e 's/.*(\(.*\)base.* .*/\1/')
-    if [[ $speed -ge 750 ]]; then
-        if ((output_tmux)); then
-            color="#[fg=$good_color]"
-        else
-            color=$good_color
-        fi
-
-        # Yellow
-    elif [[ $speed -ge 250 ]] && [[ $speed -lt 750 ]]; then
-        if ((output_tmux)); then
-            color="#[fg=$middle_color]"
-        else
-            color=$middle_color
-        fi
-
-        # Red
-    elif [[ $speed -lt 250 ]]; then
-        if ((output_tmux)); then
-            color="#[fg=$warn_color]"
-        else
-            color=$warn_color
-        fi
-    fi
-    sparks=$(spark 0 ${speed} 1000)
-    graph=${sparks:1:1}
-    echo " ${currentservice} ${color}[${speed} Mbit/s] ${graph} #[default]"
+    echo "$(wifi.sh -t)"
 else
-    echo ""
+    echo "$(ethernet.sh ${sdev})"
 fi
--- vim/after/syntax/python.vim
+++ vim/after/syntax/python.vim
@@ -1,2 +1,8 @@
 highlight pythonClass ctermfg=03 guifg=#f0c674
-syntax match pythonClass "\<[A-Z]\+[a-zA-z0-9]\+[a-z0-9]\+\>"
+syntax match pythonClass "\<[A-Z][a-zA-Z0-9]\+\>"
+
+highlight def link pythonSelf Identifier
+syntax match pythonSelf "\<self\>"
+
+highlight def link pythonCls Identifier
+syntax match pythonCls "\<cls\>"
