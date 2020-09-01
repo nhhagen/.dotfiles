d52494c352f31ba19d9f056472375a275fd33e58
Niels Henrik Hagen <nhhagen@gmail.com>
2020-09-01 12:22:41 +0200

    Configure Dock to autohide

--- scripts/dock-config.sh
+++ scripts/dock-config.sh
@@ -5,5 +5,6 @@ set -x
 defaults write com.apple.dock static-only -bool TRUE
 defaults write com.apple.dock showhidden -bool TRUE
 defaults write com.apple.dock tilesize -integer 32
+defaults write com.apple.dock autohide -integer 1
 
 killall Dock
